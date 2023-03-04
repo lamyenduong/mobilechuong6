a00bac354bdac141c5cb7f0467c7102a6d49c5e9
Elysia <lamyenduong.798@gmail.com>
2023-03-04 15:14:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobilechuong6/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mobilechuong6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+    private AdapterView.OnItemClickListener onitemclick = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent intent = new Intent(getBaseContext(), ViewPhotoActivity.class);
+            intent.putExtra("id", gridView.getAdapter().getItemId(position));
+            startActivity(intent);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridView);
+        PhotoAdapter photoAdapter = new PhotoAdapter(PhotoData.generatePhotoData(), getApplicationContext());
+        gridView.setAdapter(photoAdapter);
+        gridView.setOnItemClickListener(onitemclick);
+    }
+}
